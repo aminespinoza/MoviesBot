036951b0f5325b9e240d4f0389d0c4c8120ffc16
Amin Espinoza <doomsday5@hotmail.com>
2018-10-18 10:05:02 -0500

    Project upload

---
+++ moviepediabot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=moviepediabot-Web-Deploy.pubxml -p:Password=wEJk9vwGPoykGhckQyrfseRziaoK1aGsXo5Jkg1kFhrWqfuoWDh4zkEjmCu9
+
